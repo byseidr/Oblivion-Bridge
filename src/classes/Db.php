959cb2bd3ff9e485b0b7a896d37d23494a1aa317
Seidr <seidr@byseidr.com>
2023-12-25 16:12:35 -0300

    Fix Oblivion\Db conn property not being accessed in the proper way

--- src/classes/Db.php
+++ src/classes/Db.php
@@ -8,14 +8,14 @@ class Db
 
     function __construct()
     {
-        $conn = mysqli_connect($_ENV['DATABASE_HOSTNAME'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD'], $_ENV['DATABASE_NAME']);
+        $this->conn = mysqli_connect($_ENV['DATABASE_HOSTNAME'], $_ENV['DATABASE_USERNAME'], $_ENV['DATABASE_PASSWORD'], $_ENV['DATABASE_NAME']);
     }
     function query($query)
     {
-        return mysqli_query($conn, $query);
+        return mysqli_query($this->conn, $query);
     }
     function error()
     {
-        return mysqli_error($conn);
+        return mysqli_error($this->conn);
     }
 }
