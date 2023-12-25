50336e51316452e2b37df8e3d0a943ef31128321
Seidr <seidr@byseidr.com>
2023-12-25 01:12:59 -0300

    Include once all files in Oblivion\Oblivion::init
    With this, avoid future problems of non-intentionally redeclaring things.

--- src/Oblivion.php
+++ src/Oblivion.php
@@ -8,16 +8,16 @@ class Oblivion
 {
     public static function init($env)
     {
-        include __DIR__ . '/includes/db.php';
-        include __DIR__ . '/includes/filter.php';
-        include __DIR__ . '/includes/session.php';
+        include_once __DIR__ . '/includes/db.php';
+        include_once __DIR__ . '/includes/filter.php';
+        include_once __DIR__ . '/includes/session.php';
 
         $dir = __DIR__ . '/classes/';
         $dir = dir($dir);
         while ($arquivo = $dir->read()) {
             if ($arquivo == ".." || $arquivo == ".") {
             } else {
-                include '' . __DIR__ . '/classes/' . $arquivo . '';
+                include_once '' . __DIR__ . '/classes/' . $arquivo . '';
             }
         }
         $dir->close();
